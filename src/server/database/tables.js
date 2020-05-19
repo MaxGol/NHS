4df2351ec11a7fe1fd0ef1efc1d0c8306b0b68e3
Max G <golubev.maksim@gmail.com>
2020-05-19 09:47:15 +0100

    adding user query by phone

--- src/server/database/tables.js
+++ src/server/database/tables.js
@@ -5,3 +5,4 @@ export const SESSION = `nhs-chat-bots-${process.env.STAGE}-session`
 export const ADMIN_EMAIL_INDEX = 'EmailIndex'
 export const SAVED_BY_USER_INDEX = 'SavedByUserIndex'
 export const APPROVED_CONTENT_INDEX = 'ApprovedIndex'
+export const USER_PHONE_INDEX = 'UserPhoneIndex'
